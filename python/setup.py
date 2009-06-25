50bfb4384a042464fee8b7cc496aa6771afc9857
Michael Giarlo <leftwing@alumni.rutgers.edu>
2009-06-25 23:53:01 +0000

    tightens up setup.py a bit
    git-svn-id: https://library-callnumber-lc.googlecode.com/svn/trunk@20 cf210cc4-13c3-11de-bcda-2bb9902f8029

--- python/setup.py
+++ python/setup.py
@@ -1,27 +1,24 @@
-from setuptools import setup, find_packages
-
-install_requires = []
+from setuptools import setup
 
 classifiers = """
-Intended Audience :: Developers
-Intended Audience :: Information Technology
-License :: OSI Approved :: MIT License
-Programming Language :: Python
-Development Status :: 4 - Beta
-Topic :: Text Processing :: General
-Topic :: Utilities
+    Intended Audience :: Developers
+    Intended Audience :: Information Technology
+    License :: OSI Approved :: MIT License
+    Programming Language :: Python
+    Development Status :: 4 - Beta
+    Topic :: Text Processing :: General
+    Topic :: Utilities
 """
 
 setup(
     name = 'callnumber',
+    description = 'normalize Library of Congress call numbers and create ranges of normalized call numbers',
     version = '0.1.0',  # remember to update callnumber/__init__.py on release!
     url = 'http://code.google.com/p/library-callnumber-lc/',
     author = 'Michael J. Giarlo',
     author_email = 'leftwing@alumni.rutgers.edu',
     license = 'http://www.opensource.org/licenses/mit-license.php',
-    packages = find_packages(),
-    install_requires = install_requires,
-    description = 'normalize Library of Congress call numbers and create ranges of normalized call numbers',
-    classifiers = filter(None, classifiers.split('\n')),
+    packages = ['callnumber'],
     test_suite = 'test',
-    )
+    classifiers = [c.strip() for c in classifiers.splitlines() if c],
+)
