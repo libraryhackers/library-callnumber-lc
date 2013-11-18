3299c0673bf35c96db74f8af3de615124926b7a5
Galen Charlton <gmc@esilibrary.com>
2013-11-18 22:34:53 +0000

    Revert "deletes files from googlecode repo, updates README to reflect the Python project having migrated to Github"
    After a discussion with Michael Giarlo, we're putting the Python
    and Perl versions back in the same repo.
    
    This reverts commit 6f5bbc3d4947c60939dcbabe8433462c47a45dac.

---
+++ python/test.py
@@ -0,0 +1,11 @@
+import unittest
+from test import main
+
+
+def suite():
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(main.suite())
+    return test_suite
+
+runner = unittest.TextTestRunner()
+runner.run(suite())
