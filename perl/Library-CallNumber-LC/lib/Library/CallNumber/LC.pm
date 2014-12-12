5ce1d7ab5cca7533bad406f37cc3e718ac37c0fb
libsys-admin <libsys-admin@poptop.(none)>
2014-12-12 12:46:05 -0500

    Adjust Perl changelog, bump .pm version number
    Also, commit some auto build bit changes
    
    Signed-off-by: Dan Wells <dbw2@calvin.edu>

--- perl/Library-CallNumber-LC/lib/Library/CallNumber/LC.pm
+++ perl/Library-CallNumber-LC/lib/Library/CallNumber/LC.pm
@@ -10,11 +10,11 @@ Library::CallNumber::LC - Deal with Library-of-Congress call numbers
 
 =head1 VERSION
 
-Version 0.22;
+Version 0.23;
 
 =cut
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 
 =head1 SYNOPSIS
